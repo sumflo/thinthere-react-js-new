70fa1fa49f76095a0c0333c031fec4569acd044a
sumflo <real61madrid@gmail.com>
2021-12-03 23:09:45 +0100

    navbar-searching bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Navbar } from 'react-bootstrap'
 import { Container } from 'react-bootstrap'
 import { Nav } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
+import { FormControl } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 function NavBar(){
     return(
         <Navbar expand="lg" bg="light" variant="light">
-            <Container>
+            <Container className="navbar-container">
                 <Navbar.Brand>thinThere</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -17,6 +20,15 @@ function NavBar(){
                         <Nav.Link href="">Join Us</Nav.Link>
                         <Nav.Link href="/admin">Admin</Nav.Link>
                     </Nav>
+                    <Form className="flex-margin">
+                        <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                        />
+                        <Button variant="outline-secondary">Search</Button>
+                    </Form>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
---
+++ src/components/ShopBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
+
+function ShopBar(){
+    return(
+        <Navbar>
+        </Navbar>
+
+    )
+}
+
+export default ShopBar
